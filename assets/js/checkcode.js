369b22bf23a18823720913d7ae81cc5c286d3f59
yunping <yunp3012@gmail.com>
2022-12-24 02:44:29 +0800

    Update checkcode.js

--- assets/js/checkcode.js
+++ assets/js/checkcode.js
@@ -20,7 +20,7 @@
 
     function createCode(code1){
         code="";
-        var codeLength = 4;
+        var codeLength = 6;
         var checkCode =$(code1);
         var random = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
 
