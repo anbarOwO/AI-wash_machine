cd5c548803fef02c606663eb8a302df286c7426b
Daniel <102491759+anbarOwO@users.noreply.github.com>
2022-12-23 15:50:50 +0800

    123

--- assets/js/point.js
+++ assets/js/point.js
@@ -1,16 +1,28 @@
 /*計算碳點區間*/ 
-
-
 function count(){
     var fin_chart1_p=parseInt(document.getElementById("point").innerText)/1000;
     var max_chart1 =Math.ceil(fin_chart1_p)*1000
     var min_chart1 =Math.floor(fin_chart1_p)*1000
+    document.getElementById("max").innerText=min_chart1
+    document.getElementById("min").innerText=max_chart1
+    
 }
 
 /*進度條*/
 function run(){
     var fin_chart1_p=parseInt(document.getElementById("point").innerText)/1000;
-    alert(fin_chart1_p)
+    var fin_chart1=document.getElementById("fin_chart1")
+    var width=0;
+    if(fin_chart1_p>1){
+        fin_chart1_p=(fin_chart1_p-Math.floor(fin_chart1_p))*100;
+       
+    }
+    else{
+        fin_chart1_p=fin_chart1_p*100;
+      
+    }
+    width+=fin_chart1_p
+    fin_chart1.style.width = width +"%";
 }
 
 
