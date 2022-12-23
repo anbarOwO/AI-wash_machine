a78329bcd4a9fdfc476aaf2d4c1bf87ad9130b38
Daniel <102491759+anbarOwO@users.noreply.github.com>
2022-12-24 03:45:39 +0800

    結束!!晚安各位

--- assets/js/Y_warrant.js
+++ assets/js/Y_warrant.js
@@ -1,6 +1,6 @@
 function agree(){  
     if(document.getElementById("one-checkbox").checked){
-        alert('hi')
+        window.location.href = "";
     }
     else{
         alert("請閱讀完授權說明，在框框處打勾")
