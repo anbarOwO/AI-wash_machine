fa621c5bca279f1393ed295cd2cc83626905504b
Daniel <102491759+anbarOwO@users.noreply.github.com>
2022-12-23 16:11:36 +0800

    finish QQQQ累死

--- assets/js/point.js
+++ assets/js/point.js
@@ -1,18 +1,13 @@
-/*計算碳點區間*/ 
-function count(){
+window.onload = function (){
     var fin_chart1_p=parseInt(document.getElementById("point").innerText)/1000;
     var max_chart1 =Math.ceil(fin_chart1_p)*1000
     var min_chart1 =Math.floor(fin_chart1_p)*1000
+    /*進度條*/
     document.getElementById("max").innerText=min_chart1
     document.getElementById("min").innerText=max_chart1
-    
-}
-
-/*進度條*/
-function run(){
-    var fin_chart1_p=parseInt(document.getElementById("point").innerText)/1000;
     var fin_chart1=document.getElementById("fin_chart1")
     var width=0;
+    /*計算碳點區間*/ 
     if(fin_chart1_p>1){
         fin_chart1_p=(fin_chart1_p-Math.floor(fin_chart1_p))*100;
        
@@ -26,16 +21,5 @@ function run(){
 }
 
 
-/*function count(){
-    var fin_chart1 = document.getElementsByClassName("fin_chart1");
-    var fin_chart1_p=document.getElementById("point").innerText;
-    alert(fin_chart1_p);
-   if (fin_chart1_p/500>1){
-        fin_chart1_p=fin_chart1_p-1
-        alert(fin_chart1_p)
-    }
-    else{
-        alert(fin_chart1_p)
-    }
-}*/
+
 
