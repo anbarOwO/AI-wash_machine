c651cde6632ded666aec192b25b034166122c8f2
Daniel <102491759+anbarOwO@users.noreply.github.com>
2022-12-24 17:12:36 +0800

    剩下RWD了QQ

--- assets/js/change_data.js
+++ assets/js/change_data.js
@@ -2,15 +2,23 @@ function change(){
     document.getElementById("t-name").readOnly=false;
     document.getElementById("t-email").readOnly=false;
     var add=document.getElementById("add")
+    var add_btn=document.getElementById("add_but")
     document.getElementById("t-adress").readOnly=false;
     add.style.visibility = "visible"
-    document.getElementById('t-btn').onclick = function() {
-    this.type = 'submit';
-    this.value = "確認修改資料";
-    this.onclick=function(){
-        this.type = 'button';
-        this.value = "編輯修改資料";
-        window.location.href = "";
+    add_btn.style.visibility = "visible"
+    document.getElementById('t-btn').type = 'submit';
+    document.getElementById('t-btn').value = "確認修改資料";
+    document.getElementById('t-btn').onclick=function(){
+        /*檢查是否是否是電子郵件 */
+        var email = document.getElementById("t-email").value;
+        if(email.includes("@")){
+            window.location.href = "";
         }
-    }
+        else{
+            alert("請輸入正確的電子郵件")
+        }
+        
+        }
+    
 }
+
