71e395df3cc9aa1763b6da673004a3cf81d4c4b2
Daniel <102491759+anbarOwO@users.noreply.github.com>
2022-12-26 21:14:22 +0800

    1

---
+++ assets/js/Y_before_teach.js
@@ -0,0 +1,13 @@
+function next(){
+    var pic =document.getElementById("pic").innerHTML;
+    var i = 1;
+    if(i<4){
+        pic.innerText="<img src='./assets/img/"+i+".jpg'>"
+        i=i+1
+        alert(pic.innerHTML="<img src='./assets/img/"+i+".jpg'>"+i)
+    }
+    {
+        i=1
+    }
+  
+}
