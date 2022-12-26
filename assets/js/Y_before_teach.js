3e57f8869710a5c1c46e5b6c4d23f8bb712982bc
Daniel <102491759+anbarOwO@users.noreply.github.com>
2022-12-26 23:01:35 +0800

    別問我這JS怎麼運行的:)

--- assets/js/Y_before_teach.js
+++ assets/js/Y_before_teach.js
@@ -1,13 +1,21 @@
-function next(){
-    var pic =document.getElementById("pic").innerHTML;
-    var i = 1;
-    if(i<4){
-        pic.innerText="<img src='./assets/img/"+i+".jpg'>"
-        i=i+1
-        alert(pic.innerHTML="<img src='./assets/img/"+i+".jpg'>"+i)
-    }
-    {
-        i=1
-    }
-  
+var slideIndex = 0;
+var btn_t =0;
+function next() {
+document.getElementById("first_pic") .style.display="none"
+  var i;
+  var x = document.getElementsByClassName("img_slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  btn_t++;
+  if(btn_t==x.length){
+    document.getElementById("next").value = "關閉"
+    document.getElementById("next").onclick =function(){
+        window.location.href = "index.html";
+    } 
+  }
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
