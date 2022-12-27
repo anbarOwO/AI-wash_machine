1ecd48dd8593b9f7410614ba7015a111d82c48e6
Daniel <102491759+anbarOwO@users.noreply.github.com>
2022-12-27 18:32:20 +0800

    1231456

--- assets/js/Y_warrant.js
+++ assets/js/Y_warrant.js
@@ -1,6 +1,6 @@
 function agree(){  
     if(document.getElementById("one-checkbox").checked){
-        window.location.href = "";
+        window.location.href = "./L_before_teach.html";
     }
     else{
         alert("請閱讀完授權說明，在框框處打勾")
