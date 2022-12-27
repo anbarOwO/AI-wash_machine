814535e3900850709c6c0d8ee9136deb6ff24e95
Daniel <102491759+anbarOwO@users.noreply.github.com>
2022-12-28 00:09:25 +0800

    25

---
+++ assets/js/change_but.js
@@ -0,0 +1,10 @@
+var change = setTimeout(change , 10000);
+document.getElementById("t-lineberbtn").onclick=function(){
+    window.location.href = "./T_recheckcode .html";
+}
+function change(){
+    document.getElementById("t-lineberbtn").innerText="完成訂單"
+    document.getElementById("t-lineberbtn").onclick=function(){
+        window.location.href = "./T_checkout.html";
+    }
+}
