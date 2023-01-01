2cff3015faacb0a7b679eff9c342dd2eefcc97b4
Daniel <102491759+anbarOwO@users.noreply.github.com>
2023-01-02 01:54:26 +0800

    努力中

--- assets/js/change_data.js
+++ assets/js/change_data.js
@@ -1,11 +1,8 @@
 function change(){
     document.getElementById("t-name").readOnly=false;
     document.getElementById("t-email").readOnly=false;
-    var add=document.getElementById("add")
-    var add_btn=document.getElementById("add_but")
+
     document.getElementById("t-adress").readOnly=false;
-    add.style.visibility = "visible"
-    add_btn.style.visibility = "visible"
     document.getElementById('t-btn').type = 'submit';
     document.getElementById('t-btn').value = "確認修改資料";
     document.getElementById('t-btn').onclick=function(){
