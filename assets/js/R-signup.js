49b936597b3166cd72435d96927d9fc949c58326
DengRuFang <guava8787@yahoo.com>
2023-01-02 15:50:37 +0800

    註冊前端完成

--- assets/js/R-signup.js
+++ assets/js/R-signup.js
@@ -1,21 +1,52 @@
-var password = document.getElementById("password");
-var passwordcheck = document.getElementById("passwordcheck");
-var btn = document.getElementById("signbutton");
+var R_password = document.getElementById("R_password");
+var R_passwordcheck = document.getElementById("R_passwordcheck");
+var R_btn = document.getElementById("R_signbutton");
+var R_accont = document.getElementById("R_account");
 
-btn.addEventListener('click',function(){
-    if(password.value==""||passwordcheck.value==""){
-        alert("不能空白!"); 
+var R_phonenum = document.getElementById("R_phonenum");
+var R_phnumbtn = document.getElementById("R_phnumbtn");
+var R_phnumcheck = document.getElementById("R_phnumcheck");
+
+
+function R_random(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+}
+var R_ran = R_random(1000,9999);
+
+R_phnumbtn.addEventListener('click',function(){
+    if(R_phonenum.value==""||!isNaN(R_phonenum.value)==false){
+        alert("請輸入電話號碼(只能輸入數字!)");
+        R_phonenum.value="";
     }
     else{
-        if(password.value == passwordcheck.value){
-            alert("註冊成功");
-            location.href='order.html';
+        R_phnumcheck.style.display="inline";
+        alert("您的電話號碼是："+R_phonenum.value+"  已傳送驗證碼為"+R_ran+"，請在30分鐘內輸入驗證碼");
+    }
+})
+
+
+R_btn.addEventListener('click',function(){
+    if(R_password.value==""||R_passwordcheck.value==""||R_accont.value==""){
+        alert("帳號密碼不能空白!"); 
+    }
+    else{
+        if(R_ran!=R_phnumcheck.value){
+            alert("驗證碼輸入錯誤，請再輸入一次");
+            R_phnumcheck.value="";
         }
         else{
-            alert("密碼和確認密碼錯誤請重新輸入一次")
-            password.value ="";
-            passwordcheck.value="";
-    
+            if(R_password.value == R_passwordcheck.value){
+                alert("註冊成功");
+                location.href='L_menu.html';
+            }
+            else{
+                alert("密碼和確認密碼錯誤請重新輸入一次")
+                R_password.value ="";
+                R_passwordcheck.value="";
+            }
         }
     }
 })
+
+
+
