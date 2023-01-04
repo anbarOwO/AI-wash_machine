b7beb5e27a7d76b6a938d52a1ba16b8bdac3e7e8
Daniel <102491759+anbarOwO@users.noreply.github.com>
2023-01-04 23:35:21 +0800

    25

--- assets/js/change_but.js
+++ assets/js/change_but.js
@@ -1,9 +1,13 @@
-var change = setTimeout(change, 1000);
+<<<<<<< HEAD
+var change = setTimeout(change, 10000);
+=======
+var change = setTimeout(change , 10000);
+>>>>>>> parent of f935ae1 (25)
 document.getElementById("t-lineberbtn").onclick=function(){
     window.location.href = "./T_recheckcode .html";
 }
 function change(){
-    document.getElementById("t-lineberbtn").innerText="完成訂單";
+    document.getElementById("t-lineberbtn").innerText="完成訂單"
     document.getElementById("t-lineberbtn").onclick=function(){
         window.location.href = "./T_checkout.html";
     }
