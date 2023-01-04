b475dcedc52ac741eebb7ce368ae7e1519eab86f
Daniel <102491759+anbarOwO@users.noreply.github.com>
2023-01-05 00:32:15 +0800

    完成(趴

--- assets/js/change_but.js
+++ assets/js/change_but.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 var change = setTimeout(change, 10000);
-=======
-var change = setTimeout(change , 10000);
->>>>>>> parent of f935ae1 (25)
-=======
-var change = setTimeout(change , 10000);
->>>>>>> parent of f935ae1 (25)
 document.getElementById("t-lineberbtn").onclick=function(){
     window.location.href = "./T_recheckcode .html";
 }
@@ -15,4 +7,13 @@ function change(){
     document.getElementById("t-lineberbtn").onclick=function(){
         window.location.href = "./T_checkout.html";
     }
+}
+window.onload=function(){
+    var out =document.getElementById("out").innerText
+    var btn2 =document.getElementById("t-lineberbtn2")
+    if(out=="代領代收"){
+        btn2.style.pointerEvents="fill";
+        btn2.style.background="#63a5a5";
+    }
+    
 }
