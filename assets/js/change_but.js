f935ae1c6ef42d8a7696d29a764040777cd3cd76
Daniel <102491759+anbarOwO@users.noreply.github.com>
2023-01-04 23:28:10 +0800

    25

--- assets/js/change_but.js
+++ assets/js/change_but.js
@@ -1,9 +1,9 @@
-var change = setTimeout(change , 10000);
+var change = setTimeout(change, 1000);
 document.getElementById("t-lineberbtn").onclick=function(){
     window.location.href = "./T_recheckcode .html";
 }
 function change(){
-    document.getElementById("t-lineberbtn").innerText="完成訂單"
+    document.getElementById("t-lineberbtn").innerText="完成訂單";
     document.getElementById("t-lineberbtn").onclick=function(){
         window.location.href = "./T_checkout.html";
     }
